ecdbd3fa18708785e9dae676fb68ebd40cd979f7
idkhow2type <lecaominhhn@gmail.com>
2022-11-16 07:51:15 +0700

    did everything else

---
+++ js/script.js
@@ -0,0 +1,27 @@
+function setImage(name) {
+    const img = document.querySelector('#slide');
+    img.classList.toggle('rotate');
+    const event = (e) => {
+        img.removeEventListener('transitionend', event);
+        img.style.backgroundImage = `url(asset/${name})`;
+        img.classList.toggle('rotate');
+    };
+    img.addEventListener('transitionend', event);
+}
+
+let img = 1;
+const imgInterval = setInterval(() => {
+    img++;
+    setImage(`${img}.jpg`);
+    if (img > 15) clearInterval(imgInterval);
+}, 1000);
+
+let heart = 0;
+const heartInterval = setInterval(() => {
+    const div = document.createElement('div');
+    div.style.left = `${Math.floor(Math.random() * 100)}vw`;
+    div.classList.add('heart');
+    document.body.appendChild(div);
+    heart++;
+    if (heart > 50) clearInterval(heartInterval);
+}, 500);
