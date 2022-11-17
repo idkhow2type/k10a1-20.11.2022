82cf153393a8f9cad4e4b0fbb63213d45b5ac3b4
idkhow2type <lecaominhhn@gmail.com>
2022-11-17 15:43:23 +0700

    wait for all images to load before begining slide

--- js/script.js
+++ js/script.js
@@ -1,19 +1,49 @@
-function setImage(name) {
-    const img = document.querySelector('#slide');
-    img.classList.toggle('rotate');
-    const event = (e) => {
-        img.removeEventListener('transitionend', event);
-        img.style.backgroundImage = `url(asset/${name})`;
-        img.classList.toggle('rotate');
-    };
-    img.addEventListener('transitionend', event);
+function setSlide(img) {
+    const oldImg = document.querySelector('img');
+    oldImg.classList.remove('loading')
+    oldImg.classList.toggle('rotate');
+    oldImg.addEventListener('transitionend', () => {
+        try {
+            // there seems to be an error here but it still works
+            // i will just put a try-catch here
+            // and hope someone creates a pr and fix it for me
+            document.querySelector('.wrapper').replaceChild(img, oldImg);
+            oldImg.classList.toggle('rotate');
+        } catch (error) {
+            console.warn(error);
+        }
+    });
 }
 
-let img = 1;
-const imgInterval = setInterval(() => {
-    img = (img % 16) + 1;
-    setImage(`${img}.jpg`);
-}, 1000);
+function onImagesLoad(arr, event) {
+    var loaded = arr.length;
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].complete) loaded--;
+        else {
+            arr[i].addEventListener('load', function () {
+                loaded--;
+                if (loaded == 0) {
+                    event();
+                }
+            });
+        }
+        if (loaded == 0) event();
+    }
+}
+
+const imgs = new Array(16);
+for (let i = 0; i < imgs.length; i++) {
+    const img = document.createElement('img');
+    img.src = `asset/${i + 1}.jpg`;
+    imgs[i] = img;
+}
+onImagesLoad(imgs, () => {
+    let iImg = 0;
+    setInterval(() => {
+        setSlide(imgs[iImg]);
+        iImg = (iImg + 1) % 16;
+    }, 1000);
+});
 
 let heart = 0;
 const heartInterval = setInterval(() => {
